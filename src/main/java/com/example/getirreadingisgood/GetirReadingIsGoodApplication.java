afd2085bbc8a5a1b6c13f374ba57914b22fcd0fc
Cagdas Han Yilmaz <cagdasyilmaz96@gmail.com>
2022-02-05 00:49:43 +0300

    Spring boot project is initialized

---
+++ src/main/java/com/example/getirreadingisgood/GetirReadingIsGoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.getirreadingisgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetirReadingIsGoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetirReadingIsGoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/getirreadingisgood/GetirReadingIsGoodApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.getirreadingisgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetirReadingIsGoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
